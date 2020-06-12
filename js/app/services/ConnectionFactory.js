3de5f2a403159c31862c52c3ea9af12d57a5acb2
Bruno Rezende <brunoc.rez@gmail.com>
2020-06-12 13:24:49 -0300

    applying module pattern on connection factory

--- js/app/services/ConnectionFactory.js
+++ js/app/services/ConnectionFactory.js
@@ -1,29 +1,48 @@
-var database_stores = ['trades'];
-var database_version = 1;
-var database_name = 'trade_system';
+var ConnectionFactory = (function() {
 
-class ConnectionFactory {
+    var database_stores = ['trades'];
+    var database_version = 1;
+    var database_name = 'trade_system';
+    var connection = null;
 
-    constructor() {
-        throw new Error('Connection Factory cannot be instantiated!');
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error('Connection Factory cannot be instantiated!');
+        }
+
+        static getConn() {
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(database_name, database_version);
+
+                openRequest.onupgradeneeded = event => {
+                    ConnectionFactory._createStore(event.target.result);
+                };
+
+                openRequest.onsuccess = event => {
+                    if (!connection) {
+                        connection = event.target.result;
+                    }
+
+                    resolve(connection);
+                };
+
+                openRequest.onerror = event => {
+                    console.log(event.target.error);
+                    reject(event.target.error.name);
+                };
+            });
+        }
+
+        static _createStore(conn) {
+            database_stores.forEach(item => {
+                if (conn.objectStoreNames.contains(item)) {
+                    conn.deleteObjectStore(item);
+                }
+
+                conn.createObjectStore(item, { autoIncrement: true });
+            });
+        }
     }
-
-    static getConn() {
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(database_name, database_version);
-
-            openRequest.onupgradeneeded = event => {
-
-            };
-
-            openRequest.onsuccess = event => {
-
-            };
-
-            openRequest.onerror = event => {
-
-            };
-        });
-    }
-}
+})();
